0d30efcd68bf1d1b2baeb4fa8ab07243396de032
NiciDieNase <mail@nicidienase.de>
2012-02-13 12:48:58 +0100

    added generic config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('IZ_BENUTZER', ' HIER_HS-KUERZEL_EINTRAGEN ');
+define('IZ_PASSWORT', ' HIER_PASSWORT_EINTRAGEN ');
+define('FILE_LETZTER_NOTENSPIEGEL', dirname(__FILE__) . '/letzter_notenspiegel.csv');
+define('BENACHRICHTUNGS_EMAILADRESSE', 'mail@EXAMPLE.COM');
+define('ERROR_EMAILADRESSE', 'error@EXAMPLE.COM');
+?>
